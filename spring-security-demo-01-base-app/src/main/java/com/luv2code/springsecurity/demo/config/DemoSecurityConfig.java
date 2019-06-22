8d9934cdbefc3545b79bc6616fb075a6a50451c8
Franklin Castillo <franklincastillo18@gmail.com>
2019-06-21 22:58:29 -0400

    Spring Security, custom user registration demo with custom + db authentication. Use of validators, hibernate, etc

--- spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -3,13 +3,14 @@ package com.luv2code.springsecurity.demo.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,8 +19,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	//add a reference to our security data source
 	@Autowired
 	private DataSource securityDataSource;
-	
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
@@ -63,6 +63,5 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	        .exceptionHandling().accessDeniedPage("/access-denied"); 
     }
 	
-
 	
 }
