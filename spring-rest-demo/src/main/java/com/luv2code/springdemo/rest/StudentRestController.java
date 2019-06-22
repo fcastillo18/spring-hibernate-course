b0faad26e9186b9e9dfc16f97fdc36414c116a1c
Franklin Castillo <franklincastillo18@gmail.com>
2019-06-22 15:57:34 -0400

    Spring Rest, getStudents() and getStudent(id) available , also there is a @PostConstruct annotation to load data at first code of the bean

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,47 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	private List<Student> listStudent;
+	
+	//define @PostContruct to load the student data...only once
+	@PostConstruct
+	public void loadData() {
+		listStudent = new ArrayList<Student>();
+		
+		listStudent.add(new Student("Franklin", "Castillo"));
+		listStudent.add(new Student("Andy", "Ramos"));
+		listStudent.add(new Student("Mary", "Patel"));
+	}
+	
+	
+	//define endpoint for /students
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
+		return listStudent;
+	}
+	
+	//define endpoint for /students/{studentId} return student by index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId){ //this param name has to be the same as the one define in get mapping
+		
+		return listStudent.get(studentId);
+	}
+	
+	
+}
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -12,6 +12,8 @@
 	<hr>
 	
 	<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+	<br><br>
+	<a href="${pageContext.request.contextPath}/api/students">Get all Students</a>
 	
 </body>
 </html>
