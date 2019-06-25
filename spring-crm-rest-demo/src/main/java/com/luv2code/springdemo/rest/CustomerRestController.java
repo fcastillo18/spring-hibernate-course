d0aa7dbae7e705876dde31fd8075839596b114bd
Franklin Castillo <franklincastillo18@gmail.com>
2019-06-24 21:44:58 -0400

    Spring Rest, update and delete customer using PUTH and DELETE from the REST Api

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,4 +53,28 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
+	//Add mapping for PUT/Customers - update a  customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer){
+
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	//Add mapping for DELETE/Customers/{customerId}
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId){
+		
+		Customer customer = customerService.getCustomer(customerId);
+		
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found - "+ customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer id - "+customerId;
+	}
+		
 }
