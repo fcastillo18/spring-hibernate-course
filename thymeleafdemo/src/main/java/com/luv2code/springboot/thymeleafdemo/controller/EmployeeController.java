ed17a0552e98f4ccdc5ef2454117ada479dc8baf
Franklin Castillo <franklincastillo18@gmail.com>
2019-07-03 19:18:00 -0400

    Thymeleaf-Spring Boot: udpdate options completly available

--- thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -37,6 +38,16 @@ public class EmployeeController {
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model model) {
 		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id,
+									Model model) {
+		Employee employee = employeeService.findById(id);
 		
 		model.addAttribute("employee", employee);
 		
