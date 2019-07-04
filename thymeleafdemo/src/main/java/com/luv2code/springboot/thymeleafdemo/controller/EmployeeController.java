1b0a797ba3243842e50dd98679e1ca96828bf85a
Franklin Castillo <franklincastillo18@gmail.com>
2019-07-03 21:29:17 -0400

    END***Thymeleaf-Spring Boot: delete option completly available

--- thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,11 +48,20 @@ public class EmployeeController {
 	@GetMapping("/showFormForUpdate")
 	public String showFormForUpdate(@RequestParam("employeeId") int id,
 									Model model) {
+		
 		Employee employee = employeeService.findById(id);
 		
 		model.addAttribute("employee", employee);
 		
 		return "employees/employee-form";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int id) {
+		
+		employeeService.deleteById(id);
+		
+		return "redirect:/employees/list";
 	}
 	
 	@PostMapping("/save")
